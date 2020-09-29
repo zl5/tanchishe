8283e2f9447c58a1753c50a36575593407babdad
刘卓 <2351482107@qq.com>
2020-09-29 17:25:54 +0800

    创建了一条蛇

--- js/index.js
+++ js/index.js
@@ -70,20 +70,39 @@ window.onload = function() {
     //创建蛇的方法
     function Snack(map) {
         //设置蛇的高宽
-        this.height = map.atom;
+
         this.width = map.atom;
+        this.height = map.atom;
         //设置蛇的一个默认方向
-        this.direction = 'right'
+        this.direction = 'right';
         this.body = [
             { x: 2, y: 0 }, //蛇头，第一节
             { x: 1, y: 0 }, //蛇脖子，第二节
-            { x: 0, y: 0 }, //蛇尾，第三节
+            { x: 0, y: 0 } //蛇尾，第三节
         ];
         //显示蛇
         this.display = function() {
             for (var i = 0; i < this.body.length; i++) {
-                var she = document.createElement('div');
-                this.body[i].flag = she;
+                if (this.body[i].x != null) { //当吃到食物时，x==null,不能创建  不然会在0,0处新建一个
+                    var she = document.createElement('div');
+                    this.body[i].flag = she;
+
+                    //设置蛇的样式
+                    she.style.width = this.width + 'px';
+                    she.style.height = this.height + 'px';
+
+                    she.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 20) + "," + Math.floor(Math.random() * 288) + "," + Math.floor(Math.random() * 288) + ")";
+
+
+                    //设置蛇的位置
+                    she.style.position = 'absolute';
+                    she.style.left = this.body[i].x * this.width + 'px';
+                    she.style.top = this.body[i].y * this.height + 'px';
+                    //把蛇追加到地图中
+                    map.canvas.appendChild(she);
+
+                }
+
 
             }
         }
@@ -94,6 +113,9 @@ window.onload = function() {
     var map = new map(20, 30, 30);
     map.create();
     var food = new Food(map);
+
+    var snake = new Snack(map);
+    snake.display();
 
 
     var timer;
--- js/jjj.js
+++ js/jjj.js
@@ -47,22 +47,64 @@ window.onload = function() {
 
     function Food(map) {
 
-
         this.width = map.atom;
         this.height = map.atom;
-        this.bgcolor = "rgb(" + Math.floor(Math.random() * 20) + "," + Math.floor(Math.random() * 288) + "," + Math.floor(Math.random() * 280) + ")"
-        this.x = Math.floor(Math.random() * map.xnum);
-        this.y = Math.floor(Math.random() * map.ynum);
+        // 生成的食物是随机色
+        this.bgcolor = "rgb(" + Math.floor(Math.random() * 20) + "," + Math.floor(Math.random() * 288) + "," + Math.floor(Math.random() * 288) + ")"
+            // 食物出现的位置   随机
+        this.x = Math.floor(Math.random() * map.xnum)
+        this.y = Math.floor(Math.random() * map.ynum)
         this.flag = document.createElement('div');
-        this.flag.style.width = this.width + 'px ';
+        this.flag.style.width = this.width + 'px';
         this.flag.style.height = this.height + 'px';
+
         this.flag.style.backgroundColor = this.bgcolor;
-        // is.flag.style.borderRadius = ‘58 % '；
         this.flag.style.position = 'absolute';
         this.flag.style.left = this.x * this.width + 'px';
         this.flag.style.top = this.y * this.height + 'px';
+        // 把创建的flag添加到画布中
         map.canvas.appendChild(this.flag);
 
+
+    }
+    //创建蛇的方法
+    function Snack(map) {
+        //设置蛇的高宽
+        this.height = map.atom;
+        this.width = map.atom;
+        //设置蛇的一个默认方向
+        this.direction = 'right'
+        this.body = [
+            { x: 2, y: 0 }, //蛇头，第一节
+            { x: 1, y: 0 }, //蛇脖子，第二节
+            { x: 0, y: 0 } //蛇尾，第三节
+        ];
+        //显示蛇
+        this.display = function() {
+            for (var i = 0; i < this.body.length; i++) {
+                if (this.body[i].x != null) { //当吃到食物时，x==null,不能创建  不然会在0,0处新建一个
+                    var she = document.createElement('div');
+                    this.body[i].flag = she;
+
+                    //设置蛇的样式
+                    she.style.width = this.width + 'px';
+                    she.style.height = this.height + 'px';
+
+                    she.style.backgroundColor = "rgb(" + Math.floor(Math.random() * 20) + "," + Math.floor(Math.random() * 288) + "," + Math.floor(Math.random() * 288) + ")";
+
+
+                    //设置蛇的位置
+                    she.style.position = 'absolute';
+                    she.style.left = this.body[i].x * this.width + 'px';
+                    she.style.top = this.body[i].y * this.height + 'px';
+                    //把蛇追加到地图中
+                    map.canvas.appendChild(she);
+
+                }
+
+
+            }
+        }
 
     }
 
